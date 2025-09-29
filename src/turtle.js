9fc3996070f92dcf85d40b8b374c71c18d790ebe
atomiczagnut <atomiczagnut@gmail.com>
2025-09-29 01:59:34 -0700

    Built the basic structure of the "turtle" drawing module

--- src/turtle.js
+++ src/turtle.js
@@ -3,4 +3,41 @@
 
 function drawLSystem(p, lSystemString, startX, startY, angle, stepLength) {
     //Initialize state
-}
+    let x = startX;
+    let y = startY;
+    let angle = -90; //Facing up
+
+    //Create stack
+    let stack = [];
+
+    //Set up p5
+    p.angleMode(p.DEGREES);
+    p.stroke("#3BB143");
+
+    //Loop through each character
+    for (let char of lSystemString) {
+        if (char === 'F') {
+            //Move forward and draw
+            let newX = x + stepLength * p.cos(angle);
+            let newY = y + stepLength * p.sin(angle);
+            p.line(x, y, newX, newY);
+            x = newX;
+            y = newY;
+        } else if (char === '+') {
+            //Turn right
+            angle += turnAngle;
+        } else if (char === '-') {
+            //Turn left
+            angle -= turnAngle;
+        } else if (char === '[') {
+            //Save current state
+            stack.push({x: x, y: y, angle: angle});
+        } else if (char === ']') {
+            //Restore saved state
+            let state = stack.pop();
+            x = state.x;
+            y = state.y;
+            angle = state.angle;
+        };
+    };
+};
