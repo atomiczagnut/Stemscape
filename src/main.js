e0b89e94484da6d47247d7522948470f0176a712
atomiczagnut <atomiczagnut@gmail.com>
2025-09-29 03:46:23 -0700

    Animation of the tree (I have some ideas!)

--- src/main.js
+++ src/main.js
@@ -5,16 +5,25 @@ import { generateLSystem } from './lsystem.js';
 import { drawLSystem } from './turtle.js';
 
 const sketch = (p) => {
-    let lSystem = generateLSystem("X", { "X": "F[-X][+X]FX", "F": "FF" }, 3);
+    let lSystem;
+    let currentIteration = 0;
+    const maxIterations = 5;
+
     p.setup = () => {
         //Create canvas
         p.createCanvas(800, 600);
         p.background(0);
+        p.frameRate(1);
     };
 
     p.draw = () => {
         //animation loop
-        drawLSystem(p, lSystem, p.width/2, p.height, 25, 10);
+        if (currentIteration < maxIterations) {
+            p.background(0);
+            lSystem = generateLSystem("X", { "X": "F[-X][+X]FX", "F": "FF" }, currentIteration);
+            drawLSystem(p, lSystem, p.width/2, p.height, 25, 10);
+            currentIteration++;
+        };
     };
 };
 
